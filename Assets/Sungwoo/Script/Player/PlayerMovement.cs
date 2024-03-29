3ea7288fdbbf0bbe8b546993be400f26fc9d69e8
Chosungwoo123 <ejyoo12345@naver.com>
2024-03-29 18:58:45 +0900

    플레이어 기본 움직임 로직 제작.

--- Assets/Sungwoo/Script/Player/PlayerMovement.cs
+++ Assets/Sungwoo/Script/Player/PlayerMovement.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float turnSpeed = 10f;
+    [SerializeField] private Camera mainCamera;
+
+    private float turnSpeedMultiplier;
+
+    private bool isWalk;
+
     private Animator anim;
+
+    private Vector2 inputVec;
+    private Vector3 targetDirection;
+    private Quaternion freeRotation;
 
     private void Start()
     {
@@ -13,6 +24,60 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        
+        InputUpdate();
+        AnimationUpdate();
+        TargetDirectionUpdate();
+        RotationUpdate();
+    }
+
+    private void InputUpdate()
+    {
+        #region 이동 키 입력 로직
+
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+
+        if (inputVec != Vector2.zero)
+        {
+            isWalk = true;
+        }
+        else
+        {
+            isWalk = false;
+        }
+
+        #endregion
+    }
+
+    private void AnimationUpdate()
+    {
+        anim.SetBool("isWalk", isWalk);
+    }
+
+    private void TargetDirectionUpdate()
+    {
+        turnSpeedMultiplier = 1f;
+        var forward = mainCamera.transform.TransformDirection(Vector3.forward);
+        forward.y = 0;
+
+        var right = mainCamera.transform.TransformDirection(Vector3.right);
+
+        targetDirection = inputVec.x * right + inputVec.y * forward;
+    }
+
+    private void RotationUpdate()
+    {
+        if (isWalk && targetDirection.magnitude > 0.1f)
+        {
+            Vector3 lookDirection = targetDirection.normalized;
+            freeRotation = Quaternion.LookRotation(lookDirection, transform.up);
+            var diferenceRotation = freeRotation.eulerAngles.y - transform.eulerAngles.y;
+            var eulerY = transform.eulerAngles.y;
+
+            if (diferenceRotation < 0 || diferenceRotation > 0) eulerY = freeRotation.eulerAngles.y;
+            var euler = new Vector3(0, eulerY, 0);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(euler), turnSpeed * turnSpeedMultiplier * Time.deltaTime);
+        }
     }
 }
