5287504a8e8f858316af87fd656c1c9dcdeaaf05
Chosungwoo123 <ejyoo12345@naver.com>
2024-03-29 17:51:36 +0900

    플레이어 애니메이션 변경.

---
+++ Assets/Sungwoo/Script/Player/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
