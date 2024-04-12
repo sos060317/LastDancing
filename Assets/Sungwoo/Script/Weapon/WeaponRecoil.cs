4dcda56ae146b6e66a38a4710cb2e9ff15613dac
Chosungwoo123 <ejyoo12345@naver.com>
2024-04-12 10:57:30 +0900

    반동 패턴 제작

--- Assets/Sungwoo/Script/Weapon/WeaponRecoil.cs
+++ Assets/Sungwoo/Script/Weapon/WeaponRecoil.cs
@@ -8,9 +8,13 @@ public class WeaponRecoil : MonoBehaviour
     [HideInInspector] public CinemachineVirtualCamera playerCamera;
     [HideInInspector] public CinemachineImpulseSource cameraShake;
 
-    public float verticalRecoil;
+    public Vector2[] recoilPattern;
     public float duration;
 
+    private int recoilIndex = 0;
+
+    private float verticalRecoil;
+    private float horizontalRecoil;
     private float time = 0;
 
     private CinemachinePOV aim;
@@ -22,9 +26,21 @@ public class WeaponRecoil : MonoBehaviour
         cameraShake = GetComponent<CinemachineImpulseSource>();
     }
 
+    private int NextIndex()
+    {
+        return (recoilIndex + 1) % recoilPattern.Length;
+    }
+
     public void GenerateRecoil()
     {
         time = duration;
+
+        horizontalRecoil = recoilPattern[recoilIndex].x;
+        verticalRecoil = recoilPattern[recoilIndex].y;
+
+        recoilIndex = NextIndex();
+
+        // 시네머신 쉐이크
         cameraShake.GenerateImpulse();
     }
 
@@ -34,6 +50,7 @@ public class WeaponRecoil : MonoBehaviour
         if (time > 0)
         {
             aim.m_VerticalAxis.Value -= (verticalRecoil * Time.deltaTime) / duration;
+            aim.m_HorizontalAxis.Value -= (horizontalRecoil * Time.deltaTime) / duration;
             time -= Time.deltaTime;
         }
     }
