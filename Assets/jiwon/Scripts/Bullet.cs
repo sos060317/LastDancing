879a10ccf3583a56fb2a244dbc14e404b885d9dc
woojiwon <woojiwon060317@gmail.com>
2024-04-16 06:28:25 +0900

    적 패턴 구현

--- Assets/jiwon/Scripts/Bullet.cs
+++ Assets/jiwon/Scripts/Bullet.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private float moveSpeed = 10f;
+
     private void Start()
     {
-        Destroy(gameObject, 1f);
+        Destroy(gameObject, 5f);
+    }
+
+    private void Update()
+    {
+        transform.Translate(Time.deltaTime * moveSpeed * Vector3.up);
     }
 
     private void OnTriggerEnter(Collider other)
--- Assets/jiwon/Scripts/Enemy/EnemyShotgun.cs
+++ Assets/jiwon/Scripts/Enemy/EnemyShotgun.cs
@@ -22,20 +22,55 @@ public class EnemyShotgun : EnemyBase
         if (!isAttackReady || fireTimer <= fireDelay)
             return;
 
-        // »êÅº ÃÑ¾Ë ¹ß»ç
-        for (int i = 0; i < pelletCount; i++)
-        {
-            // ÃÑ¾ËÀÌ ³ª°¥ °¢µµ °è»ê
-            float randomSpread1 = Random.Range(0, -spreadAngle);
-            float randomSpread2 = Random.Range(0, -spreadAngle);
-            Quaternion bulletDirection = Quaternion.Euler(90 + randomSpread1, randomSpread2, 0);
-
-            // ÃÑ¾Ë »ý¼º
-            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, bulletDirection);
-
-            bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * fireSpeed;
-        }
+        TargetingSingleShot();
+        anim.SetTrigger(ShootingHash);
 
         fireTimer = 0;
+    }
+
+    private void CircleShot(int count)
+    {
+        for (int i = 0; i < 360; i += 360 / count)
+        {
+            Instantiate(bulletPrefab, firePoint.position, Quaternion.Euler(0, i, 90));
+        }
+    }
+
+    private void CircleDelayShot(int count)
+    {
+        StartCoroutine(shotCoroutine());
+
+        IEnumerator shotCoroutine()
+        {
+            for (int i = 0; i < 360; i += 360 / count)
+            {
+                Instantiate(bulletPrefab, firePoint.position, Quaternion.Euler(0, i, 90));
+                yield return new WaitForSeconds(0.1f);
+            }
+
+            yield break;
+        }
+    }
+
+    private void TargetingSingleShot()
+    {
+        float z = Mathf.Atan2(target.position.z, target.position.x) * Mathf.Rad2Deg;
+        Quaternion rot = Quaternion.Euler(0, z, 90);
+        Instantiate(bulletPrefab, firePoint.position, rot);
+    }
+
+    private void MovingCircleShot(int count)
+    {
+        float radius = 2f;
+        for (int i = 0; i < 360; i += 360 / count)
+        {
+            Vector3 pos = firePoint.position + new Vector3(Mathf.Cos(i * Mathf.Deg2Rad) * radius, 0, Mathf.Sin(i * Mathf.Deg2Rad) * radius);
+
+            Vector3 nor = (target.position - firePoint.position).normalized;
+            float y = Mathf.Atan2(nor.z, nor.x) * Mathf.Rad2Deg;
+            Quaternion rot = Quaternion.Euler(0, y, 90);
+
+            Instantiate(bulletPrefab, pos, rot);
+        }
     }
 }
