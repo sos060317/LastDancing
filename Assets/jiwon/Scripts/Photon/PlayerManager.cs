80483318eab9cbb337ded3f1682d861a79254f93
WooJiWon <woojiwon060317@gmail.com>
2024-05-22 19:02:26 +0900

    메뉴 씬과 인게임 씬 연결

--- Assets/jiwon/Scripts/Photon/PlayerManager.cs
+++ Assets/jiwon/Scripts/Photon/PlayerManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using System.IO;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -22,7 +23,6 @@ public class PlayerManager : MonoBehaviour
 
     private void CreateController()
     {
-        Debug.Log("Instantiated Player Controller");
-        // 플레이어 컨트롤러 생성
+        //PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), Vector3.zero, Quaternion.identity);
     }
 }
