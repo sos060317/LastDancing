b4bd8936ee5b3eb8c735acb42125470bb8a7032a
WooJiWon <woojiwon060317@gmail.com>
2024-06-25 14:00:31 +0900

    플레이어 닉네임 설정 및 인게임 닉네임 빌보드 구현

---
+++ Assets/jiwon/Scripts/Billboard.cs
@@ -0,0 +1,27 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    Camera myCamera;
+
+    private void Start()
+    {
+        Camera[] cameras = FindObjectsOfType<Camera>();
+
+        myCamera = cameras.FirstOrDefault(cam =>
+        {
+            PhotonView PV = cam.GetComponent<PhotonView>();
+            return PV != null && PV.IsMine;
+        });
+    }
+
+    private void Update()
+    {
+        transform.LookAt(myCamera.transform);
+        transform.Rotate(Vector3.up * 180);
+    }
+}
