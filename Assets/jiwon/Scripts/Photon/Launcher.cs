3f991a7babf0451a6e8c30465cf9d84f5819b173
woojiwon <woojiwon060317@gmail.com>
2024-07-16 23:22:33 +0900

    방 겹쳐서 생성되는 버그 수정

--- Assets/jiwon/Scripts/Photon/Launcher.cs
+++ Assets/jiwon/Scripts/Photon/Launcher.cs
@@ -5,6 +5,7 @@ using Photon.Pun;
 using TMPro;
 using Photon.Realtime;
 using System.Linq;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
@@ -19,6 +20,8 @@ public class Launcher : MonoBehaviourPunCallbacks
     [SerializeField] private GameObject playerListItemPrefab;
     [SerializeField] private GameObject startGameButton;
     [SerializeField] private TMP_Dropdown stageDropdown;
+
+    private Dictionary<string, RoomInfo> cachedRoomList = new Dictionary<string, RoomInfo>();
 
     private void Awake()
     {
@@ -157,6 +160,9 @@ public class Launcher : MonoBehaviourPunCallbacks
     /// </summary>
     public void StartGame()
     {
+        Hashtable customProperty = new Hashtable();
+        customProperty["IsGameStarted"] = true;
+        PhotonNetwork.CurrentRoom.SetCustomProperties(customProperty);
         PhotonNetwork.LoadLevel(1);
         //PhotonNetwork.LoadLevel("Stage" + stageDropdown.value);
     }
@@ -197,18 +203,37 @@ public class Launcher : MonoBehaviourPunCallbacks
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
         // 방 초기화
-        foreach(Transform trans in roomListContent)
+        foreach (Transform trans in roomListContent)
         {
             Destroy(trans.gameObject);
         }
-        //(roomList[i].CustomProperties.ContainsKey("isGameStarted") &&
-        //        (bool)roomList[i].CustomProperties["isGameStarted"])
-        // 입장 가능한 방 표시
-        for (int i = 0; i < roomList.Count; i++)
+
+        // 방 목록 업데이트
+        foreach (var info in roomList)
         {
-            if (roomList[i].RemovedFromList)
+            if (info.RemovedFromList)
+            {
+                cachedRoomList.Remove(info.Name);
+            }
+            else
+            {
+                cachedRoomList[info.Name] = info;
+            }
+        }
+
+        // 입장 가능한 방 표시
+        foreach (var info in cachedRoomList.Values)
+        {
+            if (info.CustomProperties != null &&
+                info.CustomProperties.ContainsKey("IsGameStarted") &&
+                (bool)info.CustomProperties["IsGameStarted"])
+            {
+                Debug.Log("true");
                 continue;
-            Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+            }
+
+            Debug.Log("false");
+            Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(info);
         }
     }
 
