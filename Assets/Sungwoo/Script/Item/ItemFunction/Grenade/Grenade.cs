e4677f58d70ce30d5e26433036b1ee34e113d327
Chosungwoo123 <ejyoo12345@naver.com>
2024-07-16 09:41:27 +0900

    아이템 데미지 로직 추가

--- Assets/Sungwoo/Script/Item/ItemFunction/Grenade/Grenade.cs
+++ Assets/Sungwoo/Script/Item/ItemFunction/Grenade/Grenade.cs
@@ -7,10 +7,13 @@ public class Grenade : MonoBehaviour
 {
     [SerializeField] private Grenade miniGrenade;
     [SerializeField] private GameObject explosionEffect;
+    [SerializeField] private float attackRange;
+    [SerializeField] private LayerMask enemyLayer;
 
     private int miniGrenadeCount = 0;
 
     private float explosionTime = 3f;
+    private float explosionDamage;
 
     private Rigidbody rigid;
 
@@ -34,18 +37,27 @@ public class Grenade : MonoBehaviour
                 transform.position + new Vector3(Random.Range(-0.4f, 0.4f), 0.3f, Random.Range(-0.4f, 0.4f)), Quaternion.identity)
                 .GetComponent<Grenade>();
 
-            grenade.Init(transform.position, 200f);
+            grenade.Init(transform.position, 200f, explosionDamage / 3);
         }
 
         // 적들 데미지 주기 
-        // ...
+        Collider[] enemies = Physics.OverlapSphere(transform.position, attackRange, enemyLayer);
+
+        foreach (var enemy in enemies)
+        {
+            if (enemy.TryGetComponent(out EnemyBase enemyBase))
+            {
+                enemyBase.TakeDamage(explosionDamage);
+            }
+        }
 
         PhotonNetwork.Destroy(gameObject);
     }
 
-    public void Init(int grenadeCount)
+    public void Init(int grenadeCount, float damage)
     {
         rigid = GetComponent<Rigidbody>();
+        explosionDamage = damage;
 
         // 날리기
         rigid.AddForce(new Vector3(Random.Range(-1f, 1f), 1, Random.Range(-1f, 1f)) * 4, ForceMode.Impulse);
@@ -60,14 +72,22 @@ public class Grenade : MonoBehaviour
     /// <summary>
     /// 미니 수류탄 초기화 함수
     /// </summary>
-    public void Init(Vector3 point, float force)
+    public void Init(Vector3 point, float force, float damage)
     {
         rigid = GetComponent<Rigidbody>();
+        explosionDamage = damage;
 
         rigid.AddExplosionForce(force, point, 100f);
 
         miniGrenadeCount = 0;
 
         StartCoroutine(ExplosionRoutine());
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 }
